8cc428d3700b97bbb6330d83dc9d8cbfe589a7c0
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-08-02 23:31:03 +0200

    changed script.js db.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-let pokeArray = [
-    {
-        Card: {
-            name: "name",
-            type: "type",
-            image: URL,
-            "background-color": "color",
-            id: "id",
-            dialog: [
-                {
-                    attack: "attack",
-                    defense: "defense",
-                },
-            ],
-        },
-    },
-];
-
 // let baseUrl = https://pokeapi.co/api/v2/;
---
+++ scripts/db.js
@@ -0,0 +1,109 @@
+let pokeArray = [
+    {
+        pokémon: {
+            name: "name",
+            type: "type",
+            image: URL,
+            "background-color": "color",
+            id: "id",
+            dialog: [
+                {
+                    name: "name",
+                    type: "type",
+                    image: URL,
+                    "background-color": "color",
+                    id: "id",
+                    info: [
+                        {
+                            about: {
+                                species: "species",
+                                Height: 170,
+                                weight: 90.5,
+                                abilities: "abilities",
+                            },
+                            baseStats: {
+                                key: "value",
+                                key: "value",
+                                key: "value",
+                            },
+                            Gender: {
+                                key: "value",
+                            },
+                            shiny: "value",
+                        },
+                    ],
+                },
+            ],
+        },
+        pokémon: {
+            name: "name",
+            type: "type",
+            image: URL,
+            "background-color": "color",
+            id: "id",
+            dialog: [
+                {
+                    name: "name",
+                    type: "type",
+                    image: URL,
+                    "background-color": "color",
+                    id: "id",
+                    info: [
+                        {
+                            about: {
+                                species: "species",
+                                Height: 170,
+                                weight: 90.5,
+                                abilities: "abilities",
+                            },
+                            baseStats: {
+                                key: "value",
+                                key: "value",
+                                key: "value",
+                            },
+                            Gender: {
+                                key: "value",
+                            },
+                            shiny: "value",
+                        },
+                    ],
+                },
+            ],
+        },
+        pokémon: {
+            name: "name",
+            type: "type",
+            image: URL,
+            "background-color": "color",
+            id: "id",
+            dialog: [
+                {
+                    name: "name",
+                    type: "type",
+                    image: URL,
+                    "background-color": "color",
+                    id: "id",
+                    info: [
+                        {
+                            about: {
+                                species: "species",
+                                Height: 170,
+                                weight: 90.5,
+                                abilities: "abilities",
+                            },
+                            baseStats: {
+                                key: "value",
+                                key: "value",
+                                key: "value",
+                            },
+                            Gender: {
+                                key: "value",
+                            },
+                            shiny: "value",
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+];
