8c89c43b793e88aedb9e78ec9232a8068f7ac053
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-08-02 23:06:45 +0200

    changed index.html script.js added .png icon.css standard.css rename .png

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let pokeArray = [];
+let pokeArray = [
+    {
+        Card: {
+            name: "name",
+            type: "type",
+            image: URL,
+            "background-color": "color",
+            id: "id",
+            dialog: [
+                {
+                    attack: "attack",
+                    defense: "defense",
+                },
+            ],
+        },
+    },
+];
 
 // let baseUrl = https://pokeapi.co/api/v2/;
