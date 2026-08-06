31723e82ebb40b3ee0e63d9341e9089a27c45fde
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-08-06 21:54:55 +0200

    changed index.html script.js style.css template.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const baseUrl = "https://pokeapi.co/api/v2/";
 
 let pkmArray = [];
 
+const dialogPkmCardsRef = document.getElementById("dialog");
+const pkmCardsRef = document.getElementById("pokemon-card");
+
 function init() {
     renderPkmCards();
 }
 
 // Daten aus der API laden mit rückgabewert, keine Ausführende Funktion
 async function loadDataApi() {
-    for (let id = 1; id <= 3; id++) {
+    for (let id = 1; id <= 9; id++) {
         let response = await fetch(baseUrl + "pokemon/" + `${id}`);
         let pkmJson = await response.json();
 
@@ -21,23 +24,34 @@ async function loadDataApi() {
 // Daten werden aus der oberen function durch den rückgabewert hier eingefügt und genutzt
 // und auch in der Template
 async function renderPkmCards() {
-    const pkmCardsRef = document.getElementById("pokemon-card");
     pkmCardsRef.innerHTML = "";
 
-    // Die Antwort der Funktion in Vaiable, da der Rückgabewert in dieser Funktion benötigt wird
+    // Die Antwort der Funktion in Variable, da der Rückgabewert in dieser Funktion benötigt wird
     let pkmData = await loadDataApi();
 
     for (let i = 0; i < pkmData.length; i++) {
         let onePkm = pkmArray[i];
 
         const pkmType = onePkm.types[0].type.name;
-        const pkmBgColor = typeColors[pkmType];
+        const pkmBgColor = typeColors[pkmType] || "#777";
 
-        pkmCardsRef.innerHTML += renderPkmCardTemp(onePkm, pkmBgColor);
+        const typesHtml = renderPkmTypeTemp(onePkm);
+
+        pkmCardsRef.innerHTML += renderPkmCardTemp(onePkm, pkmBgColor, typesHtml, i);
     }
 }
 
-function renderDialogPkmCards() {
-    const dialogPkmCardsRef = document.getElementById;
+function openDialog(index) {
     dialogPkmCardsRef.innerHTML = "";
+
+    let clickedPkm = pkmArray[index];
+    dialogPkmCardsRef.innerHTML = openDialogTemp(clickedPkm);
+
+    dialogPkmCardsRef.showModal();
+    dialogPkmCardsRef.classList.add("active");
+}
+
+function closeDialog() {
+    dialogPkmCardsRef.clsoe();
+    dialogPkmCardsRef.classList.remove("active");
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,71 @@
-function renderPkmCardTemp(onePkm, pkmBgColor) {
+function renderPkmTypeTemp(onePkm) {
+    return onePkm.types.map((t) => `<span class="type-badge ${t.type.name}">${t.type.name}</span>`).join("");
+}
+
+function renderPkmCardTemp(onePkm, pkmBgColor, typesHtml, i) {
     return /*html*/ `
-        <button class="pkm-card" style="background-color: ${pkmBgColor}">
+        <button class="pkm-card" onclick="openDialog(${i})" style="background-color: ${pkmBgColor}">
             <div class="pkm-card-nameid">
                 <h3>${onePkm.name}</h3>
                 <p>#${onePkm.id}</p>
             </div>
             <div>
-                <p>${onePkm.types[0].type.name}</p>
-                <p>${onePkm.types[1].type.name}</p>
+                ${typesHtml}
             </div>
-            <img src="${onePkm.sprites.front_shiny}" alt="">
+            <img src="${onePkm.sprites.front_shiny}" alt="onePkm.name">
         </button>
+    `;
+}
+
+function openDialogTemp(clickedPkm) {
+    return /*html*/ `
+        <section id="dialog-pkm-card" class="dialog-pkm-card">
+            <button id="exit-btn"></button>
+            <div>
+                <h3>${onePkm.name}</h3>
+                <p>#${onePkm.id}</p>
+            </div>
+            <div>
+                ${typesHtml}
+            </div>
+            <img src="${onePkm.sprites.front_shiny}" alt="onePkm.name">
+            </div>
+            <div class="dialog-tabs">
+                <button class="tab-btn active" onclick="switchTab('about')">About</button>
+                <button class="tab-btn" onclick="switchTab('stats')">Base Stats</button>
+                <button class="tab-btn" onclick="switchTab('gender')">Gender</button>
+                <button class="tab-btn" onclick="switchTab('shiny')">Shiny</button>
+            </div>
+            <div class="tab-content-container">
+                ${renderAboutTabTemp(clickedPkm)}
+            </div>
+        </section>
+    `;
+}
+
+function renderAboutTabTemp(clickedPkm) {
+    return /*html*/ `
+        <div class="tab-category">
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    `;
+}
+
+function renderStatsTemp() {
+    return /*html*/ `
+    <div class="tab-category">
+            ${clickedPkm.stats
+                .map(
+                    (s) => `
+            <div class="stats-row">
+                <span class="stats-name">${s.stats.name.toUpperCase()}:</span>
+                <span class="stats-value">${s.base_stat}</span>
+            </div>
+            `,
+                )
+                .join("")}
+        </div>
     `;
 }
