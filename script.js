146ab95044d314ea3525dce53cf3f95322e330bd
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-08-10 10:39:22 +0200

    changed index.html script.js db.js template.js dialog.css added svg´s

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://pokeapi.co/api/v2/";
 
 let pkmArray = [];
+let allPkm = [];
 
 const dialogPkmCardsRef = document.getElementById("dialog");
 const pkmCardsRef = document.getElementById("pokemon-card");
@@ -11,41 +12,153 @@ function init() {
 
 // Daten aus der API laden mit rückgabewert, keine Ausführende Funktion
 async function loadDataApi() {
-    for (let id = 1; id <= 9; id++) {
-        let response = await fetch(baseUrl + "pokemon/" + `${id}`);
-        let pkmJson = await response.json();
+    if (pkmArray.length > 0) return pkmArray;
 
-        pkmArray.push(pkmJson);
-        console.log(pkmArray);
+    for (let id = 1; id <= 6; id++) {
+        let response = await fetch(baseUrl + "pokemon/" + `${id}`);
+        let responseSpecies = await fetch(baseUrl + "pokemon-species/" + `${id}`);
+        let responseEvo = await fetch(baseUrl + "evolution-chain/" + `${id}`);
+        let pkmJson = await response.json();
+        let pkmJsonSpecies = await responseSpecies.json();
+        let pkmJsonEvo = await responseEvo.json();
+
+        pkmArray.push(pkmJson, pkmJsonSpecies, pkmJsonEvo);
+        renderSpecies(pkmJsonSpecies);
     }
     return pkmArray;
 }
 
-// Daten werden aus der oberen function durch den rückgabewert hier eingefügt und genutzt
-// und auch in der Template
-async function renderPkmCards() {
+async function fetchAllPkm() {
+    if (allPkm.length === 0) {
+        let response = await fetch(baseUrl + "pokemon?limit=1351&offset=0");
+        let allpkmJson = await response.json();
+        allPkm = allpkmJson.results;
+    }
+    return allPkm;
+}
+
+function filterPkm(list, searchVal) {
+    return list.filter((pkm) => {
+        const matchesName = pkm.name.toLowerCase().includes(searchVal);
+
+        const urlParts = pkm.url.split("/");
+        const pkmId = urlParts[urlParts.length - 2];
+        const matchesId = pkmId === searchVal;
+
+        return matchesName || matchesId;
+    });
+}
+
+async function inpSearchPkm() {
+    const searchVal = document.getElementById("search-input").value.toLowerCase().trim();
+
+    if (searchVal === "") return renderPkmCards();
+
+    const isNumber = !isNaN(searchVal);
+
+    if (!isNumber && searchVal.length < 3) return;
+    const fullList = await fetchAllPkm();
+    const foundPkm = filterPkm(fullList, searchVal);
+
+    loadSearchDetails(foundPkm);
+}
+
+function checkEmptySearch() {
+    const searchVal = document.getElementById("search-input").value.trim();
+
+    if (searchVal === "") {
+        renderPkmCards();
+    }
+}
+
+function getSearchPkmTypesHtml(onePkm) {
+    let pkmtypes = "";
+    for (let j = 0; j < onePkm.types.length; j++) {
+        pkmtypes += renderPkmTypeTemp(onePkm.types[j].type);
+    }
+    return pkmtypes;
+}
+
+function getGlobaleIndex(onePkm) {
+    let IndexResult = pkmArray.findIndex((p) => p && p.id === onePkm.id);
+    return IndexResult !== -1 ? IndexResult : 0;
+}
+
+async function loadSearchDetails(foundPkm) {
     pkmCardsRef.innerHTML = "";
 
-    // Die Antwort der Funktion in Variable, da der Rückgabewert in dieser Funktion benötigt wird
-    let pkmData = await loadDataApi();
-
-    for (let i = 0; i < pkmData.length; i++) {
-        let onePkm = pkmArray[i];
+    for (let pkm of foundPkm) {
+        const urlParts = pkm.url.split("/");
+        const pkmId = urlParts[urlParts.length - 2];
+        let detailResponse = await fetch(baseUrl + "pokemon/" + pkmId);
+        let onePkm = await detailResponse.json();
 
         const pkmType = onePkm.types[0].type.name;
         const pkmBgColor = typeColors[pkmType] || "#777";
+        let pkmtypes = getSearchPkmTypesHtml(onePkm);
+        let globalIndex = getGlobaleIndex(onePkm);
 
-        const typesHtml = renderPkmTypeTemp(onePkm);
-
-        pkmCardsRef.innerHTML += renderPkmCardTemp(onePkm, pkmBgColor, typesHtml, i);
+        pkmCardsRef.innerHTML += renderPkmCardTemp(onePkm, pkmBgColor, pkmtypes, globalIndex);
     }
+}
+
+function renderSpecies(pkmJsonSpecies) {
+    const result = pkmJsonSpecies.genera.filter((item) => item.language.name === "en");
+
+    if (result) {
+        return result.genus;
+    }
+}
+
+async function renderPkmCards() {
+    pkmCardsRef.innerHTML = "";
+    await loadDataApi();
+
+    for (let i = 0; i < pkmArray.length; i += 3) {
+        let onePkm = pkmArray[i];
+        const pkmType = onePkm.types[0].type.name;
+        const pkmBgColor = typeColors[pkmType] || "#777";
+
+        let pkmtypes = "";
+        for (let j = 0; j < onePkm.types.length; j++) {
+            pkmtypes += renderPkmTypeTemp(onePkm.types[j].type);
+        }
+        pkmCardsRef.innerHTML += renderPkmCardTemp(onePkm, pkmBgColor, pkmtypes, i);
+    }
+}
+
+function prepDialogData(index) {
+    let clickedPkm = pkmArray[index];
+    let clickedSpecies = pkmArray[index + 1];
+    let clickedEvo = pkmArray[index + 2];
+    let pkmTypesHtml = getPokemonTypesHtml(clickedPkm);
+    let genusText = getEngGenus(clickedSpecies);
+
+    return { clickedPkm, pkmTypesHtml, genusText };
+}
+
+function getPokemonTypesHtml(pokemon) {
+    let html = "";
+    for (let typeEntry of pokemon.types) {
+        html += renderPkmTypeTemp(typeEntry.type);
+    }
+    return html;
+}
+
+function getEngGenus(species) {
+    for (let item of species.genera) {
+        if (item.language.name === "en") {
+            return item.genus;
+        }
+    }
+    return "Unknown";
 }
 
 function openDialog(index) {
     dialogPkmCardsRef.innerHTML = "";
 
-    let clickedPkm = pkmArray[index];
-    dialogPkmCardsRef.innerHTML = openDialogTemp(clickedPkm);
+    const { clickedPkm, pkmTypesHtml, genusText } = prepDialogData(index);
+    dialogPkmCardsRef.innerHTML = openDialogTemp(clickedPkm, pkmTypesHtml, genusText, index);
 
     dialogPkmCardsRef.showModal();
     dialogPkmCardsRef.classList.add("active");
@@ -54,4 +167,17 @@ function openDialog(index) {
 function closeDialog() {
     dialogPkmCardsRef.close();
     dialogPkmCardsRef.classList.remove("active");
+}
+
+function changeDialogPkm(currentIndex, direction) {
+    let newIndex = currentIndex;
+
+    if (direction === "next") {
+        newIndex += 3;
+    } else if (direction === "prev") {
+        newIndex -= 3;
+    }
+    if (newIndex >= 0 && newIndex < pkmArray.length) {
+        openDialog(newIndex);
+    }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,34 +1,35 @@
-function renderPkmTypeTemp(onePkm) {
-    return onePkm.types.map((t) => `<span class="type-badge ${t.type.name}">${t.type.name}</span>`).join("");
+function renderPkmTypeTemp(type) {
+    return `<span class="type-badge ${type.name}">${type.name}</span>`;
 }
 
-function renderPkmCardTemp(onePkm, pkmBgColor, typesHtml, i) {
+function renderPkmCardTemp(onePkm, pkmBgColor, pkmtypes, i) {
     return /*html*/ `
-        <button id="card${i}" class="pkm-card" onclick="openDialog(${i})" style="background-color: ${pkmBgColor}">
+        <button id="card${i}" class="pkm-card" onclick="openDialog(${i}, 'pkmBgColor')" style="background-color: ${pkmBgColor}">
             <div class="pkm-card-nameid">
                 <h3>${onePkm.name}</h3>
                 <p>#${onePkm.id}</p>
             </div>
             <div class="pkm-types-name">
-                ${typesHtml}
+                ${pkmtypes}
             </div>
             <img id="card-image${i}" src="${onePkm.sprites.other["official-artwork"].front_shiny}" alt="onePkm.name">
         </button>
     `;
 }
 
-function openDialogTemp(clickedPkm) {
+function openDialogTemp(clickedPkm, pkmTypesHtml, genusText, index) {
     return /*html*/ `
-        <section id="overlay-dialog-name" class="dialog-overlay-dialog-name">
+        <section id="overlay-dialog-name" class="overlay-dialog-name" onclick="event.stopPropagation()">
             <button id="exit-btn"></button>
             <div>
-                <h3>${onePkm.name}</h3>
-                <p>#${onePkm.id}</p>
+                <h3>${clickedPkm.name}</h3>
+                <p>#${clickedPkm.id}</p>
             </div>
             <div>
-                ${typesHtml}
+                ${pkmTypesHtml}
             </div>
-            <img src="${onePkm.sprites.other[offical - artwork].front_shiny}" alt="onePkm.name">
+            <div>
+            <img src="${clickedPkm.sprites.other["official-artwork"].front_shiny}" alt="onePkm.name">
             </div>
             <div class="dialog-tabs">
                 <button class="tab-btn active" onclick="switchTab('about')">About</button>
@@ -36,40 +37,43 @@ function openDialogTemp(clickedPkm) {
                 <button class="tab-btn" onclick="switchTab('gender')">Gender</button>
                 <button class="tab-btn" onclick="switchTab('shiny')">Shiny</button>
             </div>
-            <div class="tab-content-container">
-                ${renderAboutTabTemp(clickedPkm)}
+            <div class="tab-content-container" id="tab-content">
+                ${renderAboutTabTemp(clickedPkm, genusText)}
                 <div>
-                    <button id="prev-button">preview</button>
-                    <button id="next-button">next</button>
+                    <button class= id="prev-button" onclick="changeDialogPkm((${index}), 'prev')">prev</button>
+                    <button id="next-button" onclick="changeDialogPkm((${index}), 'next')">next</button>
                 </div>
             </div>
         </section>
     `;
 }
 
-function renderAboutTabTemp(clickedPkm) {
+function renderAboutTabTemp(onePkm, genusText) {
     return /*html*/ `
         <div class="tab-category">
-            <p></p>
-            <p></p>
-            <p></p>
+            <table>
+                <tr>
+                    <th>Species</th>
+                    <td>${genusText}</td>
+                </tr>
+                <tr>
+                    <th>Height:</th>
+                    <td>${onePkm.height / 10} m</td>
+                </tr>
+                <tr>
+                    <th>weight</th>
+                    <td>${onePkm.weight / 10} kg</td>
+                </tr>
+            </table>
         </div>
     `;
 }
 
 function renderStatsTemp() {
     return /*html*/ `
-    <div class="tab-category">
-            ${clickedPkm.stats
-                .map(
-                    (s) => `
-            <div class="stats-row">
-                <span class="stats-name">${s.stats.name.toUpperCase()}:</span>
-                <span class="stats-value">${s.base_stat}</span>
-            </div>
-            `,
-                )
-                .join("")}
+        <div class="stats-row">
+            <span class="stats-name">${s.stat.name.toUpperCase()}:</span>
+            <span class="stats-value">${s.base_stat}</span>
         </div>
     `;
 }
